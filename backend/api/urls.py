d3bf907e85d501f40de919699f7110aaccd516d8
Victor Black <drugojkira@rambler.ru>
2024-09-24 19:45:37 +0500

    rev_8

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -16,5 +16,6 @@ router_v1.register("recipes", api_views.RecipeViewSet, basename="recipes")
 
 urlpatterns = [
     path("", include(router_v1.urls)),
+    path('', include('djoser.urls')),
     path("auth/", include("djoser.urls.authtoken")),
 ]
