5c2dc297c34d56de616edf2a79be533679ead73c
Victor Black <drugojkira@rambler.ru>
2024-10-12 00:59:24 +0500

    rev

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,7 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'recipes.FoodgramUser'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
