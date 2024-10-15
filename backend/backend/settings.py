64cb8c6504dfaa68a7d3f9c13e3867e604a470f0
Victor Black <drugojkira@rambler.ru>
2024-10-15 21:45:02 +0500

    net

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/backend_static/'
-STATIC_ROOT = BASE_DIR / 'backend_static'
+STATIC_ROOT = '/app/backend_static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -147,7 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'recipes.FoodgramUser'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/app/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
